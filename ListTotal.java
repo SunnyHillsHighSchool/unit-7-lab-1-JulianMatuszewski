39bec396bce70287d4150d3a7f0ea66539c02beb
Julian Matuszewski <600018222@fjuhsd.org>
2021-01-28 17:36:23 +0000

    added static total method

--- ListTotal.java
+++ ListTotal.java
@@ -8,8 +8,22 @@ import java.util.ArrayList;
 
 public class ListTotal
 {
- public static int total( List<Integer> ray )
+
+ //Julian Matuszewski
+ //AP Computer Science, Period 2
+ //precondition: ArrayList is not null
+ public static int total( List<Integer> list )
  {
-  return 0;
+  //Inst/Dec int sum = 0
+  int sum = 0;
+  //Instantiate new Integer object i in
+  //for-each loop (i: list)
+  for (Integer i: list)
+  {
+    //sum += i.intValue()
+    sum += i;
+  }
+  //return sum
+  return sum;
  }
 }
